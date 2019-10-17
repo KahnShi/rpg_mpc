e26d0676c9013d22ab51276bd209f9f7721dc3db
KahnShi <shifan@jsk.imi.i.u-tokyo.ac.jp>
2019-10-18 03:10:20 +0900

    Add mpc keyboard command to send target state.

---
+++ script/mpc_keyboard_command.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+import rospy
+import sys
+import time
+import math
+
+from std_msgs.msg import Empty
+from nav_msgs.msg import Odometry
+
+import sys, select, termios, tty
+
+msg = """
+Reading from the keyboard  and Publishing to Twist!
+---------------------------
+1:             preset waypoint 1, relative dist: (1.0, 1.0, 0.5)
+2:             preset waypoint 2, relative dist: (1.0, -1.0, 0.5)
+q:             quit
+
+please don't have caps lock on.
+"""
+
+
+class mpcTaskKeyboardInterface:
+    def init(self):
+        rospy.init_node('hydrus_mpc_task', anonymous=True)
+        self.__settings = termios.tcgetattr(sys.stdin)
+        print msg
+
+        ## pub
+        self.__mpc_target_odom_pub = rospy.Publisher('/mpc/target_odom', Odometry, queue_size=1)
+
+        #sub
+        self.__hydrus_odom = Odometry()
+        self.__hydrus_cog_odom_sub = rospy.Subscriber('/uav/cog/odom', Odometry, self.__cogOdomCallback)
+        time.sleep(0.5)
+        rospy.Timer(rospy.Duration(0.01), self.__timerCallback)
+
+    def __cogOdomCallback(self, msg):
+        self.__hydrus_odom = msg
+
+    def __sendMpcTargetOdom(self, pos_offset, period):
+        mpc_target_odom = Odometry()
+        mpc_target_odom = self.__hydrus_odom
+        mpc_target_odom.header.stamp = rospy.Time.now() + rospy.Duration(period)
+        mpc_target_odom.pose.pose.position.x += pos_offset[0]
+        mpc_target_odom.pose.pose.position.y += pos_offset[1]
+        mpc_target_odom.pose.pose.position.z += pos_offset[2]
+        self.__mpc_target_odom_pub.publish(mpc_target_odom)
+
+    def getKey(self):
+        tty.setraw(sys.stdin.fileno())
+        select.select([sys.stdin], [], [], 0)
+        key = sys.stdin.read(1)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.__settings)
+        return key
+
+    def __timerCallback(self, event):
+	key = self.getKey()
+	print "the key value is %d" % ord(key)
+	# takeoff and landing
+	if key == '1':
+            self.__sendMpcTargetOdom([1.0, 1.0, 0.5], 2.0)
+	if key == '2':
+            self.__sendMpcTargetOdom([1.0, -1.0, 0.5], 2.0)
+	if key == 'q':
+            print "force quit by visit unexisted function"
+	    error()
+
+if __name__ == '__main__':
+    try:
+        mpc_task_interface = mpcTaskKeyboardInterface()
+        mpc_task_interface.init()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+
